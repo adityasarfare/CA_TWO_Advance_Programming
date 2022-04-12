b5bc614e567a884197e467cb8913aae34f5e5ec7
Akash <akashravi55@gmail.com>
2022-04-12 20:56:06 +0100

    create students and routing added

--- studentLoginFrontEnd/studentLoginFrontEnd/src/app/app.module.ts
+++ studentLoginFrontEnd/studentLoginFrontEnd/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './student-list/student-list.component';
+import { CreateStudentComponent } from './create-student/create-student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentListComponent
+    StudentListComponent,
+    CreateStudentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
