4ca9c632fb6d934bd25b1e4e9c821207507bc8c3
aditya sarfare <adityasarfare@gmail.com>
2022-04-12 17:59:19 +0530

    Front end created with student.ts, student-list-component, student service and routing module added in it akash please create add-student component

---
+++ studentLoginFrontEnd/studentLoginFrontEnd/src/app/student.ts
@@ -0,0 +1,7 @@
+export class Student {
+    id: number | undefined;
+    firstName: string | undefined;
+    lastName: string | undefined;
+    emailId: string | undefined;
+    mobileNumber: number | undefined;
+}
